c2589f4dafaefa7b687f63fee1dbd209a1ad7b1d
Benj Sicam <benj@benjsicam.me>
2015-07-02 14:13:16 +0800

    New Event-driven Model
    New Event-driven Model

---
+++ core.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var inherits     = require('util').inherits,
+	EventEmitter = require('events').EventEmitter;
+
+function Core() {
+	if (!(this instanceof Core)) {
+		return new Core();
+	}
+
+	EventEmitter.call(this);
+	Core.init.call(this);
+}
+
+inherits(Core, EventEmitter);
+
+Core.init = function () {
+	var self = this;
+
+	process.on('message', function (event, data) {
+		if (event === 'ready')
+			self.emit('ready', data.options, data.imports);
+	});
+};
+
+module.exports = Core;
