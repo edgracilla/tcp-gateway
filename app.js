ef60ff9edabf003f2b7c66c02c7197392942862e
Benj Sicam <benj@benjsicam.me>
2015-07-03 12:28:46 +0800

    Refactored endpoint code pattern.
    Refactored endpoint code pattern.

--- app.js
+++ app.js
@@ -1,109 +1,80 @@
 'use strict';
 
-var _             = require('lodash'),
-	host          = require('ip').address(),
-	StringDecoder = require('string_decoder').StringDecoder,
-	decoder       = new StringDecoder('utf8'),
-	core          = require('./core')();
+/*
+ * Initialize the endpoint.
+ */
+var endpoint = require('./endpoint')();
 
-core.on('ready', function (options) {
-	var serverAddress = host + '' + options.port;
-	var server = require('./server')(options.port, host, {
-		_keepaliveTimeout: 3600000
-	});
+/*
+ * Listen for the ready event.
+ */
+endpoint.on('ready', function (options) {
+	var _             = require('lodash'),
+		host          = require('ip').address(),
+		StringDecoder = require('string_decoder').StringDecoder,
+		decoder       = new StringDecoder('utf8'),
+		serverAddress = host + '' + options.port,
+		server        = require('./server')(options.port, host, {
+			_keepaliveTimeout: 3600000
+		});
 
 	server.on('ready', function () {
 		console.log('TCP Server now listening on '.concat(host).concat(':').concat(options.port));
-		process.send({
-			type: 'listening'
-		});
+		endpoint.sendListeningState();
 	});
 
-	server.on('client_on', function (client) {
-		server.send(client, 'CONNACK');
-
-		process.send({
-			type: 'connection',
-			data: client
-		});
+	server.on('client_on', function (clientAddress) {
+		server.send(clientAddress, 'CONNACK');
+		endpoint.sendConnection(clientAddress);
 	});
 
-	server.on('client_off', function (client) {
-		process.send({
-			type: 'disconnect',
-			data: client
-		});
+	server.on('client_off', function (clientAddress) {
+		endpoint.sendDisconnect(clientAddress);
 	});
 
-	server.on('data', function (client, rawData) {
+	server.on('data', function (clientAddress, rawData) {
 		var data = decoder.write(rawData);
 
-		process.send({
-			type: 'data',
-			data: {
-				server: serverAddress,
-				client: client,
-				data: data
-			}
-		});
-
-		process.send({
-			type: 'log',
-			data: {
-				title: 'Raw Data Received',
-				description: data
-			}
-		});
+		endpoint.sendData(serverAddress, clientAddress, data);
+		endpoint.sendLog('Raw Data Received', data);
 	});
 
 	server.on('error', function (error) {
 		console.error('Server Error', error);
-		process.send({
-			type: 'error',
-			data: {
-				name: error.name,
-				message: error.message,
-				stack: error.stack
-			}
-		});
+		endpoint.sendError(error);
 	});
 
 	server.on('close', function () {
-		process.send({
-			type: 'close'
-		});
+		endpoint.sendClose();
 	});
 
 	server.listen();
 
-	process.on('message', function (m) {
-		if (m.type === 'message' && m.data.message) {
-			var message = m.data.message;
-
-			if (message.server === serverAddress && _.contains(_.keys(server.getClients()), message.client)) {
-				server.send(message.client, message.message, false, function (error) {
+	/*
+	 * Listen for the message event. Send these messages/commands to devices to this server.
+	 */
+	endpoint.on('message', function (message) {
+		if (message.server === serverAddress && _.contains(_.keys(server.getClients()), message.client)) {
+			server.send(message.client, message.message, false, function (error) {
+				if (error) {
+					console.log('Message Error', error);
+					endpoint.sendError(error);
+				}
+				else
+					endpoint.sendLog('Message Sent', message.message);
+			});
+		}
+		else if (message.client === '*') {
+			server.getClients().forEach(function (client) {
+				server.send(client, message.message, false, function (error) {
 					if (error) {
 						console.log('Message Error', error);
-						process.send({
-							type: 'error',
-							data: {
-								name: error.name,
-								message: error.message,
-								stack: error.stack
-							}
-						});
+						endpoint.sendError(error);
 					}
-					else {
-						process.send({
-							type: 'log',
-							data: {
-								title: 'Message Sent',
-								description: message.message
-							}
-						});
-					}
+					else
+						endpoint.sendLog('Message Sent', message.message);
 				});
-			}
+			});
 		}
 	});
 });
